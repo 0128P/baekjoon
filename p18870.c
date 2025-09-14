de5a080f1a17529b232b42ac7b6f0d6ad645a355
Naeun <pnu0128@gmail.com>
2025-09-15 00:09:27 +0900

    내일할래

---
+++ p18870.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int index;
+  int num;
+}Point;
+
+int main() {
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;++i) {
+    scanf("%d",&arr[i].num);
+  }
+
+}
